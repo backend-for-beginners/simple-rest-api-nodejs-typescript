571331a77bbd92b5643a8e5ddd2f625ad93501b3
backend-for-beginners <backendforbeginners@gmail.com>
2021-07-31 23:38:56 +0700

    setup eslint

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// import * as sourceMapSupport from "typescript";
+
+const message: string = 'Hello World';
+console.log(message);
+console.log("This is backend for beginer");
+console.log("All code run successfully");
